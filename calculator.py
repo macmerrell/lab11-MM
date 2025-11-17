49b279b84b48062235380b2a427e1bb021c21e9e
macmerrell <macattack1308@gmail.com>
2025-11-16 23:03:52 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,28 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/macmerrell/lab11-MM
+# Partner 1: Matthew Merrell
+# Partner 2: Matthew Merrell
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
 
-
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+def exp(a, b):
+    return a ** b
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)

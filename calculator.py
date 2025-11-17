7f0f9ee7c8500d3164ae902ab077328bfe96d1e1
macmerrell <macattack1308@gmail.com>
2025-11-16 23:19:09 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ def add(a, b):
     return a + b
 def sub(a, b):
     return a - b
+def subtract(a, b):
+    return a - b
 def mul(a, b):
     return a * b
 def div(a, b):
@@ -15,6 +17,10 @@ def div(a, b):
         raise ZeroDivisionError
     return b / a
 def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError
     return math.log(b, a)
